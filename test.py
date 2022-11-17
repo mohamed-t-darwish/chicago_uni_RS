d84b3372df920d66c4502b9608d6b910704d48a3
Omnia Elemary <omniaelemary@Omnias-MBP.lan>
2022-11-17 13:15:24 -0500

    updates

---
+++ test.py
@@ -0,0 +1,14 @@
+def test_1():
+    print("test1 complete")
+
+def test_2():
+    print("test2 complete")
+
+def test_3():
+    print("test3 complete")
+
+def test_4():
+    print("test4 complete")
+
+def test_5():
+    print("test5 complete")
